f94712ae8cd885ae77ef8273ae3c2ff15dc0440e
Benson Wong <mostlygeek+git@gmail.com>
2013-10-30 12:51:41 -0700

    reduce concurrency of download from 125 to 25

--- worker-combiner/combine.js
+++ worker-combiner/combine.js
@@ -18,14 +18,30 @@ var s3 = new AWS.S3();
 
 var sourceBucket = 'telemetry-published-v1' 
     , bucket = 'telemetry-test-bucket'
-    , key = 'filelists/idle_daily/Firefox/nightly/27.0a1/20130918030202.201310-1383156161.32.json';
+    , key = 'filelists/idle_daily/Firefox/nightly/27.0a1/20130918030202.201310-1383156161.32.json'
+    , workDir = '/tmp/work';
 
 fetchFragmentList(s3, bucket, key, function(err, fragList) {
     if (err) return console.log(err);
     console.log(fragList.length);
 
-    fetchObjects(s3, sourceBucket, '/tmp/work', fragList, function(err, status) {
-        if (err) return console.log(err);
-        console.log('done');
+    // create combined files locally
+    var fragment
+        , combinedObjs = [new CombinedObject(s3, sourceBucket, workDir, 0)]
+        , cObj = combinedObjs[0];
+    
+    for (i=0; i<items.length; i++) {
+        item = items[i];
+
+        if (cObj.addFragment(item) === false) {
+            // make a new one
+            cObj = new CombinedObject(s3, bucket, workDir, combinedObjs.length);
+            cObj.addFragment(item);
+            combinedObjs.push(cObj);
+        }
+    }
+
+    combinedObjs.forEach(function(obj, i) {
+        obj.debugPrint();
     });
 });
